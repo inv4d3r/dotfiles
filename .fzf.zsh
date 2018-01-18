4bff07112ff0e21d7c0be2b7869b407460d29599
Mariusz Zalewski <mariush.zalewski@gmail.com>
2018-01-18 18:08:48 +0100

    fzf key bindings scripts fixed hardcoded home path

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,14 +1,14 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/invader/.config/nvim/pack/minpac/start/fzf/bin* ]]; then
-  export PATH="$PATH:/home/invader/.config/nvim/pack/minpac/start/fzf/bin"
+if [[ ! "$PATH" == *$HOME/.config/nvim/pack/minpac/start/fzf/bin* ]]; then
+  export PATH="$PATH:$HOME/.config/nvim/pack/minpac/start/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/invader/.config/nvim/pack/minpac/start/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.config/nvim/pack/minpac/start/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/invader/.config/nvim/pack/minpac/start/fzf/shell/key-bindings.zsh"
+source "$HOME/.config/nvim/pack/minpac/start/fzf/shell/key-bindings.zsh"
 

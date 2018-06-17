5a3a4519a451c21dddfe06484679a7b03896571e
Mariusz Zalewski <mariush.zalewski@gmail.com>
2018-06-17 13:05:22 +0200

    script for creating links

---
+++ create_links.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+ln -fs "$PWD/".bash_profile ~/.bash_profile
+ln -fs "$PWD/".bashrc ~/.bashrc
+#ln -fs "$PWD/".compton.conf ~/.compton.conf
+ln -fs "$PWD/".fzf.bash ~/.fzf.bash
+ln -fs "$PWD/".fzf.zsh ~/.fzf.zsh
+ln -fs "$PWD/".gitconfig ~/.gitconfig
+ln -fs "$PWD/".inputrc ~/.inputrc
+ln -fs "$PWD/".urlview ~/.urlview
+ln -fs "$PWD/".mutt ~/.mutt
+ln -fs "$PWD/".muttrc ~/.muttrc
+ln -fs "$PWD/".mailcap ~/.mailcap
+#ln -fs "$PWD/".pentadactylrc ~/.pentadactylrc
+ln -fs "$PWD/".rtorrent.rc ~/.rtorrent.rc
+ln -fs "$PWD/".tmux.conf ~/.tmux.conf
+ln -fs "$PWD/".vimrc ~/.vimrc
+#ln -fs "$PWD/".Xdefaults ~/.Xdefaults
+ln -fs "$PWD/".xinitrc ~/.xinitrc
+ln -fs "$PWD/".Xresources ~/.Xresources
+ln -fs "$PWD/".zprofile ~/.zprofile
+ln -fs "$PWD/".zshrc ~/.zshrc

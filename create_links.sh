3dd5ec5d3ca16bcc2be491f25b8d85d2efe61205
silgurf <silgurf@tty.host>
2024-10-21 21:13:03 +0200

    refactored create_links script

--- create_links.sh
+++ create_links.sh
@@ -1,27 +1,52 @@
 #!/bin/bash
 
-ln -fs "$PWD/".bash_profile ~/.bash_profile
-ln -fs "$PWD/".bashrc ~/.bashrc
-#ln -fs "$PWD/".compton.conf ~/.compton.conf
-ln -fs "$PWD/".ctags.d ~/.ctags.d
-ln -fs "$PWD/".fzf.bash ~/.fzf.bash
-ln -fs "$PWD/".fzf.zsh ~/.fzf.zsh
-ln -fs "$PWD/".gdbinit ~/.gdbinit
-ln -fs "$PWD/".gitconfig ~/.gitconfig
-ln -fs "$PWD/".gitignore_global ~/.gitignore_global
-ln -fs "$PWD/".ignore ~/.ignore
-ln -fs "$PWD/".inputrc ~/.inputrc
-ln -fs "$PWD/".urlview ~/.urlview
-ln -fs "$PWD/".mutt ~/.mutt
-ln -fs "$PWD/".muttrc ~/.muttrc
-ln -fs "$PWD/".mailcap ~/.mailcap
-ln -fs "$PWD/".profile ~/.profile
-ln -fs "$PWD/".rtorrent.rc ~/.rtorrent.rc
-ln -fs "$PWD/".tmux.conf ~/.tmux.conf
-ln -fs "$PWD/".tridactylrc ~/.tridactylrc
-ln -fs "$PWD/".vimrc ~/.vimrc
-#ln -fs "$PWD/".Xdefaults ~/.Xdefaults
-ln -fs "$PWD/".xinitrc ~/.xinitrc
-ln -fs "$PWD/".Xresources ~/.Xresources
-ln -fs "$PWD/".zprofile ~/.zprofile
-ln -fs "$PWD/".zshrc ~/.zshrc
+set -e
+
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+function create_link {
+    if [ -z "$1" ]; then
+        echo "Error: Source file not specified"
+        return 1
+    fi
+
+    source_file="$script_dir/$1"
+    dest_file="$HOME/$1"
+    if [ -e "$dest_file" ]; then
+        echo "Warning: File $dest_file already exists. Backing up to $dest_file.bak"
+        mv "$dest_file" "$dest_file.bak"
+    fi
+    ln -sf "$source_file" "$dest_file"
+}
+
+dotfiles=(
+    ".bash_profile"
+    ".bashrc"
+    # ".compton.conf"
+    ".ctags.d"
+    ".fzf.bash"
+    ".fzf.zsh"
+    ".gdbinit"
+    ".gitconfig"
+    ".gitignore_global"
+    ".ignore"
+    ".inputrc"
+    ".urlview"
+    ".mutt"
+    ".muttrc"
+    ".mailcap"
+    ".profile"
+    ".rtorrent.rc"
+    ".tmux.conf"
+    ".tridactylrc"
+    ".vimrc"
+    # ".Xdefaults"
+    ".xinitrc"
+    ".Xresources"
+    ".zprofile"
+    ".zshrc"
+)
+
+for file in "${dotfiles[@]}"; do
+    create_link "$file"
+done
